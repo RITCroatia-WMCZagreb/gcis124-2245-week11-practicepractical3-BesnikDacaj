9c726f3dc0eb536436e1854e9cdbf1f44b11f519
Besnik Dacaj (RIT Student) <bd9794@rit.edu>
2025-04-14 12:16:58 +0200

    Done

--- src/SquareRoots.java
+++ src/SquareRoots.java
@@ -20,12 +20,15 @@ output would be:
   12.0
  */
 
-import java.util.Arrays;
-import java.util.List;
-
-public class SquareRoots {
-    public static void main(String[] args) {
-        List<Integer> squares = Arrays.asList(9, 16, -1,- 4, 625, 25, 0, 144, -81);
-       
-    }
-}
+ import java.util.Arrays;
+ import java.util.List;
+ 
+ public class SquareRoots {
+     public static void main(String[] args) {
+         List<Integer> squares = Arrays.asList(9, 16, -1, -4, 625, 25, 0, 144, -81);
+ 
+         squares.stream()
+                .filter(n -> n >= 0)
+                .forEach(n -> System.out.println(Math.sqrt(n)));
+     }
+ }
--- src/Temperature.java
+++ src/Temperature.java
@@ -13,39 +13,30 @@ public class Temperature {
      * Create and return an anonymous class of type TemperatureScale that
      * converts a temperature given in Fahrenheit to Celsius
      * C = (F - 32) / 1.8
-     * @return
      */
     public static TemperatureScale createF2C() {
-        TemperatureScale f2C = null;
-
-        // Delete from here
-       
-
-        
-        // to here
-
+        TemperatureScale f2C = new TemperatureScale() {
+            @Override
+            public double convert(double temperature) {
+                return (temperature - 32) / 1.8;
+            }
+        };
         return f2C;
     }
 
     /**
      * Part B
-     * Create and return an lambda of type TempConv that
+     * Create and return a lambda of type TemperatureScale that
      * converts a temperature given in Celsius to Fahrenheit
      * F = (C * 1.8) + 32
-     * @return
      */
     public static TemperatureScale createC2F() {
-        TemperatureScale c2F = null;
-
-        // Delete from here
-       
-        // to here
-
+        TemperatureScale c2F = (temperature) -> (temperature * 1.8) + 32;
         return c2F;
     }
 
     public static void main(String[] args) {
-        
+
         TemperatureScale c2F = createC2F();
         TemperatureScale f2C = createF2C();
 
@@ -55,33 +46,21 @@ public class Temperature {
         List<Double> fahrenheitTemps = new ArrayList<>();
 
         // Part C
-        // For each temperature in temps
-            // 1. Use c2F variable to convert the temperature to Fahrenheit and
-            //    store result in fahrenheitTemps
-            // 2. Use f2C variable to convert the temperature to Celsius and
-            //    store result in celciusTemps
-        // Delete from here
-       
-
-        // to here
+        for (double temp : temps) {
+            fahrenheitTemps.add(c2F.convert(temp));
+            celsiusTemps.add(f2C.convert(temp));
+        }
 
         // Part D
-        // Using stream(), filter(), and forEach(), print on a separate line all
-        // elements of fahrenheitTemps that are *above* feezing (32F)
-        // Delete from here
-      
-       
-      
-        // to here
+        System.out.println("Fahrenheit temps above freezing (32F):");
+        fahrenheitTemps.stream()
+                .filter(f -> f > 32)
+                .forEach(System.out::println);
 
         // Part E
-        // Using streams(), filter(), and forEach(), print on a separate line all
-        // elements of celsiusTemps that are *below* freezing (0C)
-        // Delete from here
-      
-    
-      
-        // to here
+        System.out.println("Celsius temps below freezing (0C):");
+        celsiusTemps.stream()
+                .filter(c -> c < 0)
+                .forEach(System.out::println);
     }
-    
 }
